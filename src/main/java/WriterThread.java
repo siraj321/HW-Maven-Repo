4569d0b12f14da244cbf52f55b45b19222ce44e9
Siraj <siraj321>
2020-01-07 15:12:33 +0530

    init

--- src/main/java/WriterThread.java
+++ src/main/java/WriterThread.java
@@ -1,50 +1,49 @@
-package java;
-
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class WriterThread extends Thread {
-  private ConcurrentMap<Integer, String> map;
-  private Random random;
-  private String name;
+	private ConcurrentMap<Integer, String> map;
+	private Random random;
+	private String name;
 
-  public WriterThread(ConcurrentMap<Integer, String> map,
-      String threadName, long randomSeed) {
-    this.map = map;
-    this.random = new Random(randomSeed);
-    this.name = threadName;
-  }
+	public WriterThread(ConcurrentMap<Integer, String> map, String threadName, long randomSeed) {
+		this.map = map;
+		this.random = new Random(randomSeed);
+		this.name = threadName;
+	}
 
-  public void run() {
-    while (true) {
-      Integer key = random.nextInt(10);
-      String value = name;
+	public void run() {
+		while (true) {
+			Integer key = random.nextInt(10);
+			String value = name;
 
-      if(map.putIfAbsent(key, value) == null) {
-        long time = System.currentTimeMillis();
-        String output = String.format("%d: %s has put [%d => %s]",
-            time, name, key, value);
-        System.out.println(output);
-      }
+			if (map.putIfAbsent(key, value) == null) {
+				long time = System.currentTimeMillis();
+				String output = String.format("%d: %s has put [%d => %s]", time, name, key, value);
+				System.out.println(output);
+			}
 
+			Integer keyToRemove = random.nextInt(10);
 
-      Integer keyToRemove = random.nextInt(10);
+			if (map.remove(keyToRemove, value)) {
+				long time = System.currentTimeMillis();
+				String output = String.format("%d: %s has removed [%d => %s]", time, name, keyToRemove, value);
+				System.out.println(output);
+			}
 
-      if (map.remove(keyToRemove, value)) {
-        long time = System.currentTimeMillis();
-        String output = String.format("%d: %s has removed [%d => %s]",
-            time, name, keyToRemove, value);
-        System.out.println(output);
-      }
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 
-      try {
-        Thread.sleep(500);
-      } catch (InterruptedException ex) {
-        ex.printStackTrace();
-      }
-    }
-  }
-
-  public static void main(String args[]){
-  WriterThread wt = new WriterThread();
-  wt.run();
-  }
+	public static void main(String args[]) {
+		ConcurrentMap<Integer, String> mapp = new ConcurrentHashMap<Integer, String>();
+		
+		WriterThread wt = new WriterThread(mapp, "One", 1);
+		wt.run();
+	}
 }
